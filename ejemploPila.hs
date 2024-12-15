a74680aa9c1f9b27870ed65d1f10cf2ef43878f4
Franco Spizzirri. <gtsrosario@gmail.com>
2024-12-15 17:48:31 -0300

    Update ejemploPila.hs

--- ejemploPila.hs
+++ ejemploPila.hs
@@ -16,3 +16,19 @@ module Pila (Stack,pop,push,top,emptyStk,stackIsEmpty) where
     stackIsEmpty _ = False -- con el _ indico que el stack si tiene algo.  
 
     -- en la terminar usamos : stackIsEmpty EmptyStk, push 1 EmptyStk, push 2 (push 1 EmptyStk), stackIsEmpty (push 1 EmptyStk)
+
+    -- Usando listas: 
+    newtype Stack a = Stk [a] deriving Show 
+
+    emptyStk = Stk [] 
+
+    push x (Stk xs) = Stk (x:xs) -- push pone el elemento x en el tope de la lista, o sea que siempre el q entre va a quedar primero de la lista. 
+    
+    pop (Stk []) = Stk []
+    pop (Stk (_:xs)) = xs 
+
+    top (Stk[]) = Stk []
+    top (Stk(x:_)) = x 
+
+    stackIsEmpty (Stk[]) = True 
+    stackIsEmpty (Stk _) = False 
